bc5f0dd7320ca6ab6c2cc271d9d3f253bcf4faaa
yangxg <zmrenwu@163.com>
2020-08-29 22:55:34 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
 # yufuquant-sdk
+
+yufuquant-sdk 封装了用于和 yufuquant 系统后端进行交互 API。
+
+## 安装
+
+```bash
+$ pip install yufuquant-sdk
+```
+
+## REST API 客户端
+
+REST API 客户端用于和 yufuquant 系统后台的 RESTful API 进行交互。
+
+```python
+from yufuquantsdk.clients import RESTAPIClient
+
+base_url="https://yufuquant.cc/api/v1" # 系统后端接口地址
+auth_token="xxxxx" # 认证令牌
+robot_id = 1 # 机器人 id
+rest_api_client = RESTAPIClient(base_url=base_url, auth_token=auth_token)
+
+# 获取机器人配置
+await rest_api_client.get_robot_config(robot_id)
+
+# 更新机器人的资产信息
+data = {
+  "total_balance": 5.8
+}
+await rest_api_client.patch_robot_asset_record(robot_id, data)
+
+# 发送 ping
+await rest_api_client.post_robot_ping(robot_id)
+```
+
+以下是一个完整的示例：
+
+```python
+import logging
+from pprint import pprint
+from yufuquantsdk.clients import RESTAPIClient
+
+
+# 开启日志
+logger = logging.getLogger("yufuquantsdk")
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+async def main():
+    http_client = RESTAPIClient(
+        base_url="https://yufuquant.cc/api/v1",
+        auth_token="8g2e470579ba14ea69000859eba6c421b69ff95d",
+    )
+    
+    result = await http_client.get_robot_config(robot_id=1)
+    pprint(result)
+    
+    result = await http_client.post_robot_ping(robot_id=1)
+    pprint(result)
+    
+    result = await http_client.patch_robot_asset_record(
+      robot_id=1, data={"total_balance": 10000}
+    )
+    pprint(result)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+```
+
+## Websocket API 客户端
+
+Websocket API 客户端用于和 yufuquant 系统后台的 Websocket API 进行交互。
+
+```python
+from yufuquantsdk.clients import WebsocketAPIClient
+
+uri="wss://yufuquant.cc/ws/v1/streams/" # 系统后端接口地址
+auth_token="xxxxx" # 认证令牌
+topics = ["robot#1.ping", "robot#1.log"] # 订阅的话题
+ws_api_client = WebsocketAPIClient(uri=uri)
+
+# 认证
+await ws_api_client.auth(auth_token)
+
+# 订阅话题
+await ws_api_client.sub(topics)
+
+# 取消话题订阅
+await ws_api_client.unsub(topics)
+
+# 发送 ping
+await ws_api_client.robot_ping()
+
+# 发送日志
+await ws_api_client.robot_log()
+```
+
+以下是一个完整的示例：
+
+```python
+import logging
+from yufuquantsdk.clients import WebsocketAPIClient
+
+# 开启日志
+logger = logging.getLogger("yufuquantsdk")
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+async def main():
+    ws_api_client = WebsocketAPIClient(uri="wss://yufuquant.cc/ws/v1/streams/")
+    
+    await ws_api_client.auth("8d2e470575ba04ea69000859eba6c421a69ff95c")
+    await ws_api_client.sub(topics=["robot#1.log"])
+    while True:
+        await ws_api_client.robot_ping()
+        await ws_api_client.robot_log("Test robot log...", level="INFO")
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+```
+
---
+++ yufuquantsdk/clients.py
@@ -0,0 +1,224 @@
+import asyncio
+import json
+import logging
+from datetime import datetime
+from typing import Any, Dict, Iterable, Optional, Set
+import httpx
+import websockets
+from websockets import exceptions
+
+logger = logging.getLogger("yufuquantsdk")
+
+
+class WebsocketAPIClient:
+    def __init__(self, uri: str, ws: websockets.WebSocketClientProtocol = None) -> None:
+        self._uri: str = uri
+        self._ws: websockets.WebSocketClientProtocol = ws
+        self._authed: bool = False
+        self._auth_token = ""
+        self._sub_topics: Set[str] = set()
+        self._inputs: asyncio.Queue[str] = asyncio.Queue()
+        self._outputs: asyncio.Queue[str] = asyncio.Queue(maxsize=100)
+        self._run_task: asyncio.Task[Any] = asyncio.get_event_loop().create_task(
+            self._run()
+        )
+
+    async def auth(self, auth_token: str):
+        message = {
+            "cmd": "auth",
+            "token": auth_token,
+        }
+        await self._deliver(json.dumps(message))
+        self._authed = True
+        self._auth_token = auth_token
+
+    async def sub(self, topics: Iterable[str]):
+        # Remove duplicated topics
+        if not isinstance(topics, set):
+            topics = set(topics)
+
+        message = {
+            "cmd": "sub",
+            "topics": list(topics),  # Object of type set is not JSON serializable
+        }
+        await self._deliver(json.dumps(message))
+        self._sub_topics = topics
+
+    async def unsub(self, topics: Iterable[str]):
+        # Remove duplicated topics
+        if not isinstance(topics, set):
+            topics = set(topics)
+
+        message = {
+            "cmd": "unsub",
+            "topics": list(topics),
+        }
+        await self._deliver(json.dumps(message))
+        self._sub_topics = self._sub_topics - topics
+
+    async def robot_ping(self):
+        data = {"timestamp": datetime.now().timestamp()}
+        message = {"category": "robotPing", "data": data}
+        await self._broadcast(message)
+
+    async def robot_log(self, text: str, level: str = "info"):
+        data = {
+            "text": text,
+            "level": level,
+            "timestamp": int(datetime.now().timestamp()) * 1000,
+        }
+        message = {"category": "robotLog", "data": data}
+        await self._broadcast(message)
+
+    async def _connect(self, **kwargs):
+        # disable ping
+        kwargs["ping_interval"] = None
+        try:
+            self._ws = await websockets.connect(self._uri, **kwargs)
+        except Exception as exc:
+            logger.exception("Failed to connect to %s: %s.", self._uri, exc)
+            raise
+        logger.info("Connected to %s.", self._uri)
+
+    async def _reconnect(self):
+        await self._connect()
+
+        if self._authed:
+            await self.auth(self._auth_token)
+
+        if len(self._sub_topics) > 0:
+            await self.sub(self._sub_topics)
+
+        logger.info("Reconnected to %s.", self._uri)
+
+    async def _deliver(self, s: str):
+        await self._inputs.put(s)
+
+    async def _send(self, s: str):
+        assert self._ws is not None, "No connection!"
+        try:
+            await self._ws.send(s)
+            logger.debug(">>> %s", s)
+        except websockets.ConnectionClosed as exc:
+            logger.exception(exc)
+            await self._reconnect()
+
+    async def _broadcast(self, message: Dict):
+        data = {"cmd": "broadcast", "message": message}
+        await self._deliver(json.dumps(data))
+
+    # todo: handle stop signal
+    async def _run(self):
+        await self._connect()
+        try:
+            while True:
+                incoming: asyncio.Task[Any] = asyncio.create_task(self._ws.recv())
+                outgoing: asyncio.Task[Any] = asyncio.create_task(self._inputs.get())
+
+                done: Set[asyncio.Future[Any]]
+                pending: Set[asyncio.Future[Any]]
+                done, pending = await asyncio.wait(
+                    [incoming, outgoing], return_when=asyncio.FIRST_COMPLETED
+                )
+
+                # Cancel pending tasks to avoid leaking them.
+                if incoming in pending:
+                    incoming.cancel()
+                if outgoing in pending:
+                    outgoing.cancel()
+
+                if incoming in done:
+                    try:
+                        message = incoming.result()
+                        logger.debug("<<< %s", message)
+                    except websockets.ConnectionClosed:
+                        break
+                    else:
+                        try:
+                            self._outputs.put_nowait(message)
+                        except asyncio.QueueFull:
+                            logger.warning("The outputs queue is full.")
+
+                if outgoing in done:
+                    message = outgoing.result()
+                    await self._send(message)
+        finally:
+            await self.close()
+
+    async def close(self):
+        await self._ws.close()
+        close_status = exceptions.format_close(
+            self._ws.close_code, self._ws.close_reason
+        )
+        logger.info(f"Connection closed: {close_status}.")
+
+
+ROBOT_REQ_PATH = "/robots/{robot_id}/"
+ROBOT_CONFIG_REQ_PATH = "/robots/{robot_id}/config/"
+ROBOT_PING_REQ_PATH = "/robots/{robot_id}/ping/"
+ROBOT_ASSET_RECORD_REQ_PATH = "/robots/{robot_id}/assetRecord/"
+
+
+class RESTAPIClient:
+    def __init__(self, base_url: str, auth_token: str):
+        self._base_url: str = base_url.rstrip("/")
+        self._auth_token: str = auth_token
+
+    async def get_robot_config(self, robot_id: int):
+        req_path = ROBOT_CONFIG_REQ_PATH.format(robot_id=robot_id)
+        return await self._request("GET", req_path)
+
+    async def patch_robot_asset_record(self, robot_id: int, data: Dict[str, Any]):
+        req_path = ROBOT_ASSET_RECORD_REQ_PATH.format(robot_id=robot_id)
+        return await self._request("PATCH", req_path, data=data)
+
+    async def post_robot_ping(self, robot_id: int):
+        req_path = ROBOT_PING_REQ_PATH.format(robot_id=robot_id)
+        return await self._request("POST", req_path)
+
+    async def _request(
+        self,
+        method: str,
+        req_path: str,
+        headers: Optional[Dict[str, str]] = None,
+        params: Optional[Dict[str, str]] = None,
+        data: Optional[Dict] = None,
+        auth: bool = True,
+    ):
+        req_headers = {}
+        if auth:
+            req_headers["Authorization"] = f"Token {self._auth_token}"
+        if headers is not None:
+            req_headers.update(headers)
+
+        url = self._base_url + req_path
+        async with httpx.AsyncClient() as client:
+            logger.debug(
+                "%s %s, Request<headers=%s params=%s data=%s>",
+                method,
+                url,
+                req_headers,
+                params,
+                data,
+            )
+            res = await client.request(
+                method,
+                url,
+                headers=req_headers,
+                params=params,
+                json=data,
+                timeout=5,
+            )
+            http_text = res.text
+            logger.debug(
+                "%s %s, Response<status_code=%s headers=%s http_text=%s>",
+                method,
+                url,
+                res.status_code,
+                req_headers,
+                http_text,
+            )
+        res.raise_for_status()
+        if res.status_code == "204":
+            return None
+        return res.json()
